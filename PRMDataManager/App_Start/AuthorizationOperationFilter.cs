5c1b570a9516029228799428e737be8a8e57386f
philip <philipronnmark@gmail.com>
2021-08-02 19:34:48 +0200

    Added Swagger Documentation
    Added SwashBuckle

---
+++ PRMDataManager/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,30 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace PRMDataManager.App_Start
+{
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            if(operation.paramters == null)
+            {
+                operation.parameters = new List<Parameter>();
+
+            }
+
+            operation.parameters.Add(new Paramter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "access token",
+                required = false, 
+                type = "string",
+            });
+        }
+    }
+}
