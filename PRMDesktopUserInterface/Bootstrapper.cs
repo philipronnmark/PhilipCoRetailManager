12b34c05b76e7c6839e5e5e9d19e75f002569c9d
philip <philipronnmark@gmail.com>
2021-08-04 21:15:48 +0200

    Add Login Form
    Add login form with fields.

--- PRMDesktopUserInterface/Bootstrapper.cs
+++ PRMDesktopUserInterface/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using PRMDesktopUserInterface.Helpers;
 using PRMDesktopUserInterface.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace PRMDesktopUserInterface
 {
@@ -15,6 +17,10 @@ namespace PRMDesktopUserInterface
         public Bootstrapper()
         {
             Initialize();
+            ConventionManager.AddElementConvention<PasswordBox>(
+            PasswordBoxHelper.BoundPasswordProperty,
+            "Password",
+            "PasswordChanged");
         }
 
         protected override void Configure()
