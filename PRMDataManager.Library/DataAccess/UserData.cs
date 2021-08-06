465c47cb9a10e5efee5ace38cb0451edd4af2be7
philip <philipronnmark@gmail.com>
2021-08-06 14:08:16 +0200

    Retrieve User Details
    Retrieve User details from logged in user, added api endpoint.

---
+++ PRMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,24 @@
+﻿using PRMDataManager.Library.Internal.DataAccess;
+using PRMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRMDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = Id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "PRMData");
+
+            return output;
+        }
+    }
+}
---
+++ PRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity;
+using PRMDataManager.Library.DataAccess;
+using PRMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+
+namespace PRMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+
+        //Retrives the curren logged in users' id and gets their data
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId(); 
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+        }
+
+
+       
+        }  
+}
